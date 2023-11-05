eb6f8f1f949ae2d383ff1ff8fadf0fe3627b7420
Melanie Ehrlich <melanie6104@icloud.com>
2023-11-05 01:51:58 -0500

    update email confirm screen

--- knightassist_mobile_app/lib/src/features/authentication/presentation/register/register_emailconfirmed_screen.dart
+++ knightassist_mobile_app/lib/src/features/authentication/presentation/register/register_emailconfirmed_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:knightassist_mobile_app/src/common_widgets/primary_button.dart';
 import 'package:knightassist_mobile_app/src/features/events/domain/event.dart';
 
-class EventScreen extends ConsumerWidget {
-  const EventScreen({super.key, required this.eventID});
+class ConfirmScreen extends ConsumerWidget {
+  const ConfirmScreen({super.key, required this.eventID});
   final EventID eventID;
 
   @override
@@ -12,8 +13,22 @@ class EventScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Email Confirmed'),
       ),
-      body: const Center(
-          child: Text("Thanks for confirming your email! Proceed to log in.")),
+      body: const Column(
+        children: [
+          Image(
+            image: AssetImage('assets/KnightAssistCoA3.png'),
+            height: 60,
+          ),
+          Text('Thanks for confirming your email! Please proceed to log in.',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: PrimaryButton(
+              text: 'Log In',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
