b00579a359223a544d8ff9096abb70f66f05ed31
Melanie Ehrlich <melanie6104@icloud.com>
2023-11-16 00:39:49 -0500

    added search bar and redesigned event cards

--- knightassist_mobile_app/lib/src/features/events/presentation/events_list_screen.dart
+++ knightassist_mobile_app/lib/src/features/events/presentation/events_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:knightassist_mobile_app/src/common_widgets/responsive_center.dart';
+import 'package:knightassist_mobile_app/src/constants/breakpoints.dart';
 
 class EventsListScreen extends ConsumerWidget {
   const EventsListScreen({super.key});
@@ -19,10 +21,38 @@ class EventsListScreen extends ConsumerWidget {
             Stack(
               children: [
                 _topSection(),
+                const Column(
+                  children: [
+                    SafeArea(
+                      child: Text(
+                        'Volunteer Shift Events',
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: SearchBar(
+                        hintText: 'Search Events',
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
-            const EventCard(),
-            const EventCard(),
+            Flexible(
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: const <Widget>[
+                  EventCard(),
+                  EventCard(),
+                  EventCard(),
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -48,26 +78,98 @@ class EventCard extends StatelessWidget {
 
     const style = TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
 
-    return Card(
-      shape: RoundedRectangleBorder(
-        side: const BorderSide(
-          color: Colors.black26,
-          width: 1.0,
-        ),
-        borderRadius: BorderRadius.circular(20.0),
-      ),
-      color: Colors.white,
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: ListTile(
-          title: const Text('event date', style: style),
-          subtitle: const Text('event org',
-              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 15)),
-          trailing: const Text('event time', style: style),
-          textColor: Colors.black,
-          selectedColor: theme.colorScheme.onSecondary,
-        ),
+    return SingleChildScrollView(
+      child: ResponsiveCenter(
+        maxContentWidth: Breakpoint.tablet,
+        child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                side: const BorderSide(
+                  color: Colors.black26,
+                  width: 1.0,
+                ),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              color: Colors.white,
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    OverflowBar(
+                      children: [
+                        ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: const Image(
+                                image: AssetImage('assets/example.png'),
+                                height: 100)),
+                        const Padding(
+                          padding: EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Event Title',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 18),
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                'Time/Date',
+                                style: TextStyle(fontWeight: FontWeight.w400),
+                                textAlign: TextAlign.justify,
+                              ),
+                              Text(
+                                'Location',
+                                style: TextStyle(fontWeight: FontWeight.w400),
+                                textAlign: TextAlign.justify,
+                              ),
+                              OverflowBar(
+                                children: [
+                                  ClipRRect(
+                                      child: Image(
+                                          image:
+                                              AssetImage('assets/example.png'),
+                                          height: 20)),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(
+                                      'Hosting Organization',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w400),
+                                      textAlign: TextAlign.justify,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        OverflowBar(
+                          spacing: 8,
+                          overflowAlignment: OverflowBarAlignment.end,
+                          children: [
+                            Align(
+                              alignment: const Alignment(1, 0.6),
+                              child: FilledButton(
+                                onPressed: () {},
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        const Color.fromARGB(
+                                            255, 91, 78, 119))),
+                                child: const Text('RSVP'),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )),
       ),
     );
   }
