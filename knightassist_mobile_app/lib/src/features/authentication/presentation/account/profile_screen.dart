8a01d42712af7702466a184d1f8e7e268cecfe82
Melanie Ehrlich <71104590+mel4ni3@users.noreply.github.com>
2023-11-24 15:38:28 -0500

    add editable image plugin and update profile page

--- knightassist_mobile_app/lib/src/features/authentication/presentation/account/profile_screen.dart
+++ knightassist_mobile_app/lib/src/features/authentication/presentation/account/profile_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -7,12 +9,33 @@ import 'package:knightassist_mobile_app/src/constants/breakpoints.dart';
 import 'package:knightassist_mobile_app/src/features/events/presentation/events_list_screen.dart';
 import 'package:knightassist_mobile_app/src/routing/app_router.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:editable_image/editable_image.dart';
 
-class ProfileScreen extends ConsumerWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen>{
+File? _profilePicFile;
+
+ @override
+  void initState() {
+    super.initState();
+  }
+
+Future<void> _directUpdateImage(File? file) async {
+    if (file == null) return;
+
+    setState(() {
+      _profilePicFile = file;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
@@ -49,9 +72,9 @@ class ProfileScreen extends ConsumerWidget {
       ),
       body: SizedBox(
         height: h,
-        child: Column(
+        child: ListView(
           children: [
-            Padding(
+            /*Padding(
               padding: const EdgeInsets.all(8.0),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(25.0),
@@ -61,7 +84,49 @@ class ProfileScreen extends ConsumerWidget {
                         height: 100),
               ),
             ),
-            const ResponsiveScrollableCard(child: Text('Name'))
+            const ResponsiveScrollableCard(child: Text('Name')),*/
+            EditableImage(
+                // Define the method that will run on
+                // the change process of the image.
+                onChange: _directUpdateImage,
+
+                // Define the source of the image.
+                image: _profilePicFile != null
+                    ? Image.file(_profilePicFile!, fit: BoxFit.cover)
+                    : const Image(image: AssetImage('assets/profile pictures/icon_paintbrush.png')),
+
+                // Define the size of EditableImage.
+                size: 150,
+
+                // Define the Theme of image picker.
+                imagePickerTheme: ThemeData(
+                  // Define the default brightness and colors.
+                  primaryColor: Colors.white,
+                  shadowColor: Colors.transparent,
+                  colorScheme:
+                      const ColorScheme.light(background: Colors.white70),
+                  iconTheme: const IconThemeData(color: Colors.black87),
+
+                  // Define the default font family.
+                  fontFamily: 'Georgia',
+                ),
+
+                // Define the border of the image if needed.
+                imageBorder: Border.all(color: Colors.black87, width: 2),
+
+                // Define the border of the icon if needed.
+                editIconBorder: Border.all(color: Colors.black87, width: 2),
+              ),
+               const Spacer(flex: 2),
+              _buildTextField(labelText: 'Username'),
+              const Spacer(),
+              _buildTextField(labelText: 'Full Name'),
+              const Spacer(),
+              _buildTextField(labelText: 'Email'),
+              const Spacer(),
+              _buildTextField(labelText: 'Password', obscureText: true),
+              const Spacer(flex: 2),
+              _buildTextButton(),
           ],
         )
       ),
@@ -105,3 +170,58 @@ class ProfileScreen extends ConsumerWidget {
     );
   }
 }
+
+TextField _buildTextField({String labelText = '', bool obscureText = false}) {
+    return TextField(
+      cursorColor: Colors.black54,
+      cursorWidth: 1,
+      obscureText: obscureText,
+      obscuringCharacter: '●',
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(
+          color: Colors.black54,
+          fontSize: 18,
+        ),
+        fillColor: Colors.red,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black54,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black54,
+            width: 1.5,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(40),
+          ),
+        ),
+      ),
+    );
+  }
+
+  TextButton _buildTextButton() {
+    return TextButton(
+      onPressed: () => {},
+      style: ButtonStyle(
+        padding: MaterialStateProperty.all(
+          const EdgeInsets.symmetric(vertical: 20),
+        ),
+        side:
+            MaterialStateProperty.all(const BorderSide(color: Colors.black54)),
+        backgroundColor: MaterialStateProperty.all(Colors.transparent),
+      ),
+      child: const Text(
+        'Save',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
