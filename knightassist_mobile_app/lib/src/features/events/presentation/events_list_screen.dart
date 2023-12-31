3db662f81eecd39ff3ea984ad402ab6c510e0f8a
Melanie Ehrlich <71104590+mel4ni3@users.noreply.github.com>
2023-12-31 18:23:04 -0500

    event list layout, image takes up full height of card

--- knightassist_mobile_app/lib/src/features/events/presentation/events_list_screen.dart
+++ knightassist_mobile_app/lib/src/features/events/presentation/events_list_screen.dart
@@ -8,7 +8,7 @@ import 'package:knightassist_mobile_app/src/features/events/data/events_reposito
 import 'package:knightassist_mobile_app/src/routing/app_router.dart';
 import 'package:intl/intl.dart';
 
-/*List<Event> events = [
+List<Event> events = [
   Event(
       id: '1',
       name: 'concert',
@@ -79,11 +79,11 @@ import 'package:intl/intl.dart';
       updatedAt: DateTime.now()),
     Event(
       id: '5',
-      name: 'movie night with long desc and title hahahaha hahaha wegJKHgekljbdgKLJBgdkbg;JKBglkjbasdg',
+      name: 'movie night with long desc and title hahahaha hahaha wegJKHgekljbdgKLJBgdkbg;JKBglkjbasdg eijejewqjkn',
       description: 'Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum ',
-      location: 'pegasus ballroom',
+      location: 'pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom',
       date: DateTime.fromMillisecondsSinceEpoch(1734218796000),
-      sponsoringOrganization: 'Organization Z',
+      sponsoringOrganization: 'Organization Z pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom pegasus ballroom',
       attendees: [],
       registeredVolunteers: [],
       picLink: 'assets/profile pictures/icon_cookie.png',
@@ -94,7 +94,7 @@ import 'package:intl/intl.dart';
       maxAttendees: 400,
       createdAt: DateTime.fromMillisecondsSinceEpoch(1702596396),
       updatedAt: DateTime.now()),
-];*/
+];
 
 class EventsListScreen extends ConsumerWidget {
   const EventsListScreen({super.key});
@@ -151,7 +151,7 @@ class EventsListScreen extends ConsumerWidget {
           children: [
             _topSection(w),
             Flexible(
-              child: FutureBuilder(
+              child: /*FutureBuilder(
                 future: EventsRepository().getAllEvents(),
                 builder: (ctx, snapshot) {
                   if (snapshot.connectionState == ConnectionState.done) {
@@ -172,6 +172,13 @@ class EventsListScreen extends ConsumerWidget {
                   return const Center(child: CircularProgressIndicator(),);
 
                 },
+              ),*/
+               ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: events.length,
+                itemBuilder: (context, index) => EventCard(
+                    event: events.elementAt(index)),
               ),
             )
           ],
@@ -299,12 +306,16 @@ class EventCard extends StatelessWidget {
                 onTap: () => context.pushNamed("event", extra: event),
                 child: Padding(
                   padding: const EdgeInsets.all(0.0),
-                  child: ListTile(
-                      leading: ClipRRect(
+                  child: /*ListTile(
+                      leading: /*ClipRRect(
                           borderRadius: BorderRadius.circular(12.0),
                           child: Image(
                               image: AssetImage(event.picLink),
-                              height: 50,width:50)),
+                              height: 50,width:50)),*/
+                              Container(
+                                  child: Image.asset(event.picLink,
+                                        fit: BoxFit.cover)
+                                     ),
                       subtitle: Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: Column(
@@ -369,11 +380,93 @@ class EventCard extends StatelessWidget {
                                               255, 91, 78, 119))),
                               child: const Text('RSVP'),
                             ),
-                  ),
-                ),
-              ),
+                  ),*/
+                  Container(
+                    height: 210,
+                    //color: Colors.white,
+                    child: Row(
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Container(
+                            decoration: BoxDecoration(image: DecorationImage(image: AssetImage(event.picLink), fit: BoxFit.fill)),
+                            width: 120,
+                            height: 210,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                              event.name,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 18),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              DateFormat('yyyy-MM-dd – hh:mm a')
+                                  .format(event.date),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              event.location,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400),
+                              textAlign: TextAlign.start,
+                            ),
+                            OverflowBar(
+                              children: [
+                                ClipRRect(
+                                    borderRadius:
+                                        BorderRadius.circular(25.0),
+                                    child: const Image(
+                                        image: AssetImage(
+                                            'assets/example.png'),
+                                        height: 20)),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    event.sponsoringOrganization,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w400),
+                                    textAlign: TextAlign.start,
+                                  ),
+                                ),
+                                SizedBox(width: 5,)
+                              ],
+                            ),
+                             FilledButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(
+                                          const Color.fromARGB(
+                                              255, 91, 78, 119))),
+                              child: const Text('RSVP'),
+                            ),
+                          ],
+                        ),
+                      ),
+                            ],
+                          ),
+                        ),
+                    ),
+                                  ),
             )),
-      ),
-    );
+      )
+        );
   }
 }
