3ec1a607dcd4233fedc41df4207767b9752f2fb4
Melanie Ehrlich <melanie6104@icloud.com>
2024-01-10 23:59:46 -0500

    initial feedback list screen

--- knightassist_mobile_app/lib/src/features/events/presentation/feedback_list_screen.dart
+++ knightassist_mobile_app/lib/src/features/events/presentation/feedback_list_screen.dart
@@ -16,22 +16,67 @@ import 'package:knightassist_mobile_app/src/routing/app_router.dart';
 import 'package:intl/intl.dart';
 
 List<EventFeedback> feedbacklist = [
-  EventFeedback(student: student, event: event, studentName: studentName, eventName: eventName, rating: rating, feedbackText: feedbackText, wasReadByUser: wasReadByUser, timeSubmitted: timeSubmitted, updatedAt: updatedAt),
-  EventFeedback(student: student, event: event, studentName: studentName, eventName: eventName, rating: rating, feedbackText: feedbackText, wasReadByUser: wasReadByUser, timeSubmitted: timeSubmitted, updatedAt: updatedAt),
-  EventFeedback(student: student, event: event, studentName: studentName, eventName: eventName, rating: rating, feedbackText: feedbackText, wasReadByUser: wasReadByUser, timeSubmitted: timeSubmitted, updatedAt: updatedAt),
-  EventFeedback(student: student, event: event, studentName: studentName, eventName: eventName, rating: rating, feedbackText: feedbackText, wasReadByUser: wasReadByUser, timeSubmitted: timeSubmitted, updatedAt: updatedAt),
-  EventFeedback(student: student, event: event, studentName: studentName, eventName: eventName, rating: rating, feedbackText: feedbackText, wasReadByUser: wasReadByUser, timeSubmitted: timeSubmitted, updatedAt: updatedAt)
-  
+  EventFeedback(
+      student: '1',
+      event: '1',
+      studentName: 'Johnson Doe',
+      eventName: 'concert',
+      rating: 1,
+      feedbackText: 'it was bad',
+      wasReadByUser: false,
+      timeSubmitted: DateTime.fromMillisecondsSinceEpoch(1704948441000),
+      updatedAt: DateTime.now()),
+  EventFeedback(
+      student: '2',
+      event: '2',
+      studentName: 'foo',
+      eventName: 'study session',
+      rating: 2,
+      feedbackText:
+          'Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum dolor s Lorem ipsum',
+      wasReadByUser: true,
+      timeSubmitted: DateTime.fromMillisecondsSinceEpoch(1704948441000),
+      updatedAt: DateTime.now()),
+  EventFeedback(
+      student: '3',
+      event: '2',
+      studentName: 'Test User',
+      eventName: 'movie night',
+      rating: 3,
+      feedbackText: 'idk',
+      wasReadByUser: true,
+      timeSubmitted: DateTime.fromMillisecondsSinceEpoch(1704948441000),
+      updatedAt: DateTime.now()),
+  EventFeedback(
+      student: '4',
+      event: '2',
+      studentName: 'Student',
+      eventName: 'concert',
+      rating: 4,
+      feedbackText: 'test',
+      wasReadByUser: false,
+      timeSubmitted: DateTime.fromMillisecondsSinceEpoch(1704948441000),
+      updatedAt: DateTime.now()),
+  EventFeedback(
+      student: '5',
+      event: '3',
+      studentName: '',
+      eventName: 'study session',
+      rating: 5,
+      feedbackText: 'epic',
+      wasReadByUser: true,
+      timeSubmitted: DateTime.fromMillisecondsSinceEpoch(1704948441000),
+      updatedAt: DateTime.now())
 ];
 
-class UpdateScreen extends StatefulWidget {
-  const UpdateScreen({super.key});
+class FeedbackListScreen extends StatefulWidget {
+  const FeedbackListScreen({super.key});
 
   @override
-  State<UpdateScreen> createState() => _UpdateScreenState();
+  State<FeedbackListScreen> createState() => _FeedbackListScreenState();
 }
 
-class _UpdateScreenState extends State<UpdateScreen> {
+class _FeedbackListScreenState extends State<FeedbackListScreen> {
   int _selectedIndex = 0;
   bool tapped = false;
   static const TextStyle optionStyle =
@@ -54,78 +99,9 @@ class _UpdateScreenState extends State<UpdateScreen> {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
     return Scaffold(
-      /*appBar: AppBar(
-        title: const Text(
-          'Announcements',
-          style: TextStyle(fontWeight: FontWeight.w600),
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              onPressed: () {},
-              tooltip: 'View notifications',
-              icon: const Icon(
-                Icons.notifications_outlined,
-                color: Colors.white,
-                semanticLabel: 'Notifications',
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                context.pushNamed(AppRoute.profileScreen.name);
-              },
-              child: Tooltip(
-                message: 'Go to your profile',
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(25.0),
-                  child: const Image(
-                      semanticLabel: 'Profile picture',
-                      image: AssetImage(
-                          'assets/profile pictures/icon_paintbrush.png'),
-                      height: 20),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),*/
-      body:
-          tapped ? _widgetOptions.elementAt(_selectedIndex) : UpdateScreenTab(),
-      /*Container(
-        height: h,
-        child: Column(
-          children: [
-            _topSection(w),
-            Flexible(
-              child: ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: organizations.length,
-                itemBuilder: (context, index) {
-                  int i = index;
-                  if (organizations.elementAt(index).updates.isEmpty) {
-                      return const SizedBox(height: 0,);
-                  } else {
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        physics: const ClampingScrollPhysics(),
-                        itemCount: organizations[i].updates.length,
-                        itemBuilder: (context, index) {
-                        return UpdateCard(update: organizations[i].updates.elementAt(index), sponsor: organizations[i].updates.elementAt(index).sponsor);
-                      });
-                  }
-                }
-              ),
-            )
-          ],
-        ),
-      ),*/
+      body: tapped
+          ? _widgetOptions.elementAt(_selectedIndex)
+          : FeedbackListScreenTab(),
       drawer: Drawer(
         child: ListView(
           //padding: EdgeInsets.zero,
@@ -220,7 +196,7 @@ _topSection(double width) {
                 padding: EdgeInsets.all(8.0),
                 child: Center(
                   child: SearchBar(
-                    hintText: 'Search Announcements',
+                    hintText: 'Search Feedback',
                   ),
                 ),
               ),
@@ -230,11 +206,10 @@ _topSection(double width) {
       ));
 }
 
-class UpdateCard extends StatelessWidget {
-  final Update update;
-  final Organization sponsor;
+class FeedbackCard extends StatelessWidget {
+  final EventFeedback feedback;
 
-  const UpdateCard({super.key, required this.update, required this.sponsor});
+  const FeedbackCard({super.key, required this.feedback});
 
   @override
   Widget build(BuildContext context) {
@@ -258,7 +233,8 @@ class UpdateCard extends StatelessWidget {
               color: Colors.white,
               elevation: 5,
               child: InkWell(
-                onTap: () => context.pushNamed("updatedetail", extra: update),
+                onTap: () =>
+                    context.pushNamed("feedbackdetail", extra: feedback),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
@@ -266,57 +242,35 @@ class UpdateCard extends StatelessWidget {
                     children: [
                       Padding(
                         padding: const EdgeInsets.all(0.05),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            ListTile(
-                              contentPadding: EdgeInsets.zero,
-                              dense: true,
-                              visualDensity: VisualDensity.standard,
-                              title: Text(
-                                update.title,
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w600, fontSize: 18),
-                                textAlign: TextAlign.start,
-                              ),
-                              subtitle: Wrap(children: [
-                                ClipRRect(
-                                    borderRadius: BorderRadius.circular(25.0),
-                                    child: Image(
-                                        image:
-                                            AssetImage(update.sponsor.logoUrl),
-                                        height: 25)),
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    sponsor.name,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 3,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.w400),
-                                    textAlign: TextAlign.start,
-                                  ),
-                                ),
-                              ]),
-                              trailing: Text(
-                                DateFormat('yyyy-MM-dd').format(update.date),
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w400),
-                                textAlign: TextAlign.start,
-                              ),
-                            ),
-                            Text(
-                              update.content,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          dense: true,
+                          visualDensity: VisualDensity.standard,
+                          title: Text(
+                            feedback.eventName,
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 18),
+                            textAlign: TextAlign.start,
+                          ),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              feedback.feedbackText,
                               overflow: TextOverflow.ellipsis,
                               maxLines: 3,
                               style:
                                   const TextStyle(fontWeight: FontWeight.w400),
                               textAlign: TextAlign.start,
                             ),
-                          ],
+                          ),
+                          trailing: Text(
+                            DateFormat('yyyy-MM-dd')
+                                .format(feedback.timeSubmitted),
+                            style: const TextStyle(fontWeight: FontWeight.w400),
+                            textAlign: TextAlign.start,
+                          ),
                         ),
                       ),
                     ],
@@ -329,91 +283,70 @@ class UpdateCard extends StatelessWidget {
   }
 }
 
-class UpdateScreenTab extends ConsumerWidget {
-  const UpdateScreenTab({super.key});
+class FeedbackListScreenTab extends ConsumerWidget {
+  const FeedbackListScreenTab({super.key});
 
   Widget build(BuildContext context, WidgetRef ref) {
     double h = MediaQuery.of(context).size.height;
     double w = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Announcements',
-          style: TextStyle(fontWeight: FontWeight.w600),
-        ),
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: IconButton(
-              onPressed: () {},
-              tooltip: 'View notifications',
-              icon: const Icon(
-                Icons.notifications_outlined,
-                color: Colors.white,
-                semanticLabel: 'Notifications',
-              ),
-            ),
+        appBar: AppBar(
+          title: const Text(
+            'Feedback',
+            style: TextStyle(fontWeight: FontWeight.w600),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                context.pushNamed(AppRoute.profileScreen.name);
-              },
-              child: Tooltip(
-                message: 'Go to your profile',
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(25.0),
-                  child: const Image(
-                      semanticLabel: 'Profile picture',
-                      image: AssetImage(
-                          'assets/profile pictures/icon_paintbrush.png'),
-                      height: 20),
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          actions: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: IconButton(
+                onPressed: () {},
+                tooltip: 'View notifications',
+                icon: const Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.white,
+                  semanticLabel: 'Notifications',
                 ),
               ),
             ),
-          )
-        ],
-      ),
-      body: Container(
-        height: h,
-        child: Column(
-          children: [
-            _topSection(w),
-            Flexible(
-              child: ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: organizations.length,
-                  itemBuilder: (context, index) {
-                    int i = index;
-                    if (organizations.elementAt(index).updates.isEmpty) {
-                      return const SizedBox(
-                        height: 0,
-                      );
-                    } else {
-                      return ListView.builder(
-                          shrinkWrap: true,
-                          physics: const ClampingScrollPhysics(),
-                          itemCount: organizations[i].updates.length,
-                          itemBuilder: (context, index) {
-                            return UpdateCard(
-                                update:
-                                    organizations[i].updates.elementAt(index),
-                                sponsor: organizations[i]
-                                    .updates
-                                    .elementAt(index)
-                                    .sponsor);
-                          });
-                    }
-                  }),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  context.pushNamed(AppRoute.profileScreen.name);
+                },
+                child: Tooltip(
+                  message: 'Go to your profile',
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(25.0),
+                    child: const Image(
+                        semanticLabel: 'Profile picture',
+                        image: AssetImage(
+                            'assets/profile pictures/icon_paintbrush.png'),
+                        height: 20),
+                  ),
+                ),
+              ),
             )
           ],
         ),
-      ),
-    );
+        body: Container(
+            height: h,
+            child: Column(children: [
+              _topSection(w),
+              Flexible(
+                  child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: feedbacklist.length,
+                      itemBuilder: (context, index) {
+                        return FeedbackCard(
+                          feedback: feedbacklist.elementAt(index),
+                        );
+                      }))
+            ])));
   }
 }
