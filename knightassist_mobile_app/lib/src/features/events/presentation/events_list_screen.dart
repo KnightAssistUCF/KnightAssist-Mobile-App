f443bbec602b47c8288fce3e8a5311afe76cc27e
Melanie Ehrlich <melanie6104@icloud.com>
2024-01-20 15:36:27 -0500

    organizations can edit their events from the event list screen

--- knightassist_mobile_app/lib/src/features/events/presentation/events_list_screen.dart
+++ knightassist_mobile_app/lib/src/features/events/presentation/events_list_screen.dart
@@ -28,7 +28,8 @@ List<Event> events = [
       semester: 'Fall 2023',
       maxAttendees: 1000,
       createdAt: DateTime.fromMillisecondsSinceEpoch(1700968029),
-      updatedAt: DateTime.now(), feedback: []),
+      updatedAt: DateTime.now(),
+      feedback: []),
   Event(
       id: '2',
       name: 'study session',
@@ -45,7 +46,8 @@ List<Event> events = [
       semester: 'Fall 2023',
       maxAttendees: 30,
       createdAt: DateTime.fromMillisecondsSinceEpoch(1700968029),
-      updatedAt: DateTime.now(), feedback: []),
+      updatedAt: DateTime.now(),
+      feedback: []),
   Event(
       id: '3',
       name: 'movie night',
@@ -62,7 +64,8 @@ List<Event> events = [
       semester: 'Fall 2023',
       maxAttendees: 400,
       createdAt: DateTime.fromMillisecondsSinceEpoch(1700968029),
-      updatedAt: DateTime.now(), feedback: []),
+      updatedAt: DateTime.now(),
+      feedback: []),
   Event(
       id: '4',
       name: 'movie night but its date isn\'t previous',
@@ -79,7 +82,8 @@ List<Event> events = [
       semester: 'Fall 2023',
       maxAttendees: 400,
       createdAt: DateTime.fromMillisecondsSinceEpoch(1702596396),
-      updatedAt: DateTime.now(), feedback: []),
+      updatedAt: DateTime.now(),
+      feedback: []),
   Event(
       id: '5',
       name:
@@ -100,7 +104,8 @@ List<Event> events = [
       semester: 'Fall 2023',
       maxAttendees: 400,
       createdAt: DateTime.fromMillisecondsSinceEpoch(1702596396),
-      updatedAt: DateTime.now(), feedback: []),
+      updatedAt: DateTime.now(),
+      feedback: []),
 ];
 
 class EventsListScreen extends StatefulWidget {
@@ -327,6 +332,8 @@ class EventCard extends StatelessWidget {
 
     const style = TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
 
+    bool isOrg = true;
+
     return SingleChildScrollView(
         child: ResponsiveCenter(
       maxContentWidth: Breakpoint.tablet,
@@ -490,11 +497,18 @@ class EventCard extends StatelessWidget {
                               ],
                             ),
                             FilledButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                if (isOrg) {
+                                  // only if current user is sponsoring org
+                                  context.pushNamed("editevent", extra: event);
+                                } else {
+                                  // RSVP if event not full and is student, otherwise no button (other org)
+                                }
+                              },
                               style: ButtonStyle(
                                   backgroundColor: MaterialStateProperty.all(
                                       const Color.fromARGB(255, 91, 78, 119))),
-                              child: const Text('RSVP'),
+                              child: Text(isOrg ? 'Edit' : 'RSVP'),
                             ),
                           ],
                         ),
