8b325e0ad53e32df1c2ddbcf152611882079a3e4
Melanie Ehrlich <melanie6104@icloud.com>
2024-03-27 23:18:52 -0400

    add a first time log in field to app user

--- knightassist_mobile_app/lib/src/features/authentication/domain/app_user.dart
+++ knightassist_mobile_app/lib/src/features/authentication/domain/app_user.dart
@@ -1,7 +1,12 @@
 class AppUser {
-  const AppUser({required this.id, required this.email, required this.role});
+  const AppUser(
+      {required this.id,
+      required this.email,
+      required this.role,
+      required this.firstTimeLogIn});
 
   final String id;
   final String email;
   final String role;
+  final bool firstTimeLogIn;
 }
